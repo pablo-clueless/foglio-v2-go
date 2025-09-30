3b9d5ab231eedfc1812fff6727eddae92577aacb
Samson Okunola <smsnmicheal@gmail.com>
2025-09-30 17:22:50 +0100

    feat(handlers): implement job CRUD operations and application
    add complete implementation for job creation, updating, deletion, fetching and application handling
    update gitignore to include all .env file variations

--- src/handlers/job.go
+++ src/handlers/job.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"foglio/v2/src/database"
+	"foglio/v2/src/dto"
+	"foglio/v2/src/lib"
 	"foglio/v2/src/services"
 
 	"github.com/gin-gonic/gin"
@@ -18,25 +20,109 @@ func NewJobHandler() *JobHandler {
 }
 
 func (h *JobHandler) CreateJob() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		var payload dto.CreateJobDto
+		id := ctx.Param("id")
+
+		if err := ctx.ShouldBind(&payload); err != nil {
+			lib.BadRequest(ctx, err.Error(), "400")
+			return
+		}
+
+		job, err := h.service.CreateJob(id, payload)
+		if err != nil {
+			lib.InternalServerError(ctx, "Internal server error,"+err.Error())
+			return
+		}
+
+		lib.Success(ctx, "Job created successfully", job)
+	}
 }
 
 func (h *JobHandler) UpdateJob() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		var payload dto.UpdateJobDto
+		id := ctx.Param("id")
+
+		if err := ctx.ShouldBind(&payload); err != nil {
+			lib.BadRequest(ctx, err.Error(), "400")
+			return
+		}
+
+		job, err := h.service.UpdateJob(id, payload)
+		if err != nil {
+			lib.InternalServerError(ctx, "Internal server error,"+err.Error())
+			return
+		}
+
+		lib.Success(ctx, "Job updated successfully", job)
+	}
 }
 
 func (h *JobHandler) DeleteJob() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		id := ctx.Param("id")
+
+		err := h.service.DeleteJob(id)
+		if err != nil {
+			lib.InternalServerError(ctx, "Internal server error,"+err.Error())
+			return
+		}
+
+		lib.Success(ctx, "Job updated successfully", nil)
+	}
 }
 
 func (h *JobHandler) GetJobs() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		var query dto.JobPagination
+
+		if err := ctx.ShouldBindQuery(&query); err != nil {
+			lib.BadRequest(ctx, err.Error(), "400")
+			return
+		}
+
+		jobs, err := h.service.GetJobs(query)
+		if err != nil {
+			lib.InternalServerError(ctx, "Internal server error,"+err.Error())
+			return
+		}
+
+		lib.Success(ctx, "Users fetched successfully", jobs)
+	}
 }
 
 func (h *JobHandler) GetJob() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		id := ctx.Param("id")
+
+		job, err := h.service.GetJob(id)
+		if err != nil {
+			lib.InternalServerError(ctx, "Internal server error,"+err.Error())
+			return
+		}
+
+		lib.Success(ctx, "Job updated successfully", job)
+	}
 }
 
 func (h *JobHandler) ApplyToJob() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("userId")
+		jobId := ctx.Param("jobId")
+		var payload dto.JobApplicationDto
+
+		if err := ctx.ShouldBind(&payload); err != nil {
+			lib.BadRequest(ctx, err.Error(), "400")
+			return
+		}
+
+		err := h.service.ApplyToJob(userId, jobId, payload)
+		if err != nil {
+			lib.InternalServerError(ctx, "Internal server error,"+err.Error())
+			return
+		}
+
+		lib.Success(ctx, "Job application success", nil)
+	}
 }
